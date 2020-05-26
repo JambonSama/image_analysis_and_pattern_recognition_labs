58726ede14c2d1da429a56b996e6b893b92b73e6
Claire Meyer <clairemeyerbourhis@gmail.com>
2020-05-27 01:29:18 +0200

    modified : Improved framework of the project. added : Extraction frame by frame of the arrow. modified : Merged with characters detection, in particular, only needs to add correct image extraction and char classification. modified : Cleaned all current code. modified : Commented and documented all current code.

--- project/main.py
+++ project/main.py
@@ -2,34 +2,101 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from numpy.linalg import norm
 import cv2 as cv
 
-R_H1 = 10
-R_H2 = 170
-R_S = 170
-R_V = 170
 
-B_H = 118
-B_S = 150
-B_V = 110
+def split(frame, b1, b2):
+    """
+    Splits values frome frame according to two sets of boundaries.
+    Frame has 3 channels, and is in HSV space.
+    """
+    img1 = cv.inRange(frame, (b1[0][0]-b1[0][1], b1[1][0]-b1[1][1], b1[2][0]-b1[2][1]),
+                      (b1[0][0]+b1[0][1], b1[1][0]+b1[1][1], b1[2][0]+b1[2][1]))
+    img2 = cv.inRange(frame, (b2[0][0]-b2[0][1], b2[1][0]-b2[1][1], b2[2][0]-b2[2][1]),
+                      (b2[0][0]+b2[0][1], b2[1][0]+b2[1][1], b2[2][0]+b2[2][1]))
+    return img1+img2
 
-def split_objects(frame):
-    """
-    Splits the image between the three types of objects of interest.
-    """
-    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    red1 = cv.inRange(hsv_frame, (R_H1-10, R_S-80, R_V-80), (R_H1+10, R_S+80, R_V+80))
-    red2 = cv.inRange(hsv_frame, (R_H2-10, R_S-80, R_V-80), (R_H2+10, R_S+80, R_V+80))
-    blue = cv.inRange(hsv_frame, (B_H -20, B_S-60, B_V-60), (B_H +20, B_S+60, B_V+60))
-    black = cv.inRange(hsv_frame, (0, 0, 0), (180, 250, 50))
-    return red1+red2, blue, black
 
 def detect_arrow_position(frame):
     """
     Determines the position of the arrow in the frame.
+    Frame is single channel image of the red values (arrow of robot).
     """
-    pos = (5, 4)
-    return pos, frame
+    # morphology to clean the img (a little)
+    radius = 2
+    size = 2*radius+1
+    kernel = np.zeros((size, size), np.uint8)
+    cv.circle(img=kernel, center=(radius, radius),
+              radius=radius, color=255, thickness=-1)
+    frame = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
+    frame = cv.morphologyEx(frame, cv.MORPH_CLOSE, kernel)
+
+    # connected components
+    label_number, _, stats, centroids = cv.connectedComponentsWithStats(
+        image=frame, connectivity=8)
+
+    i = 1
+
+    if label_number < 2:
+        return (-1, -1), frame
+    if label_number > 2:
+        i = np.argmax(stats[1:, cv.CC_STAT_AREA])+1
+
+    return (centroids[i][0], centroids[i][1])
+
+
+def extract_chars_img(frame, chars_pos):
+    """
+    From the list of characters positions in the frame, returns a list of the characters images.
+    Frame is single channel image of the blue and black values (characters on the area).
+    """
+    return 1
+
+
+def determine_chars(chars_img, neural_network):
+    """
+    From the list of the characters images, returns a list of the characters,
+    using the neural network for classification
+    """
+    return 1
+
+
+def detect_chars_pos(frame, robot_pos):
+    """
+    Detect where all the characters are in the frame and returns the list of it.
+    Frame is single channel image of the blue and black values (characters on the area).
+    """
+    kernel = np.zeros((3, 3), np.uint8)
+    cv.circle(img=kernel, center=(1, 1), radius=1, color=255, thickness=-1)
+
+    dilated_img = cv.morphologyEx(frame, cv.MORPH_DILATE, kernel, iterations=4)
+    contours, _ = cv.findContours(
+        image=dilated_img, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_SIMPLE)
+    chars_pos = []
+
+    for contour in contours:
+        if contour.shape[0] > 5:
+            ellipse = cv.fitEllipse(contour)
+            (x, y), (MA, ma), _ = ellipse
+            r = range(15, 60)
+            if int(MA) in r and int(ma) in r and norm((x-robot_pos[0], y-robot_pos[1])) > 20:
+                chars_pos.append((x, y))
+    return chars_pos
+
+
+def detect_chars(frame, robot_pos, neural_network):
+    """
+    From a frame, returns a list of all the chars positions, and a list of all the chars.
+    Frame is single channel image of the blue and black values (characters on the area).
+    """
+    chars_pos = detect_chars_pos(frame, robot_pos)
+    chars_img = extract_chars_img(frame, chars_pos)
+    chars = determine_chars(chars_img, neural_network)
+
+    chars = np.arange(len(chars_pos))
+    return chars_pos, chars
+
 
 def main():
     """
@@ -37,28 +104,74 @@ def main():
     """
     cap = cv.VideoCapture("../data/robot_parcours_1.avi")
 
+    # HSV boundaries for color detection
+    r1 = [[10, 10], [170, 80], [170, 80]]  # first red (around 0+ hue)
+    r2 = [[170, 10], [170, 80], [170, 80]]  # second red (around 0- hue)
+    b1 = [[118, 20], [150, 60], [110, 60]]  # blue
+    b2 = [[90, 90], [125, 125], [60, 60]]  # black
+
     # Check if camera opened successfully
     if not cap.isOpened():
         print("Error opening video stream or file")
+
+    # Frame counter
+    i = 0
+
+    # Lists of symbols
+    formula = []
+    robot_pos = []
 
     # Read until video is completed
     while cap.isOpened():
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret:
-            # Separation of robot arrow, symbols, and digits
-            arrow, symbols, digits = split_objects(frame)
+            # Dimensions of the frame
+            height = frame.shape[0]
 
-            _, arrow = detect_arrow_position(arrow)
+            # Conversion to HSV space for better color separation
+            hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-            # Display the resulting frames
-            cv.imshow("red", arrow)
-            cv.imshow("blue", symbols)
-            cv.imshow("black", digits)
+            # Find the arrow position
+            arrow_img = split(hsv_frame, r1, r2)
+            robot_pos.append(detect_arrow_position(arrow_img))
 
-            # Press Q on keyboard to  exit
-            if cv.waitKey(300) == ord("q"):
-                break
+            if i == 0:
+                chars_img = split(hsv_frame, b1, b2)
+                chars_pos, chars = detect_chars(chars_img, robot_pos[0], neural_network=1)
+
+            # Retrieve the formula
+            for i, pos in enumerate(chars_pos):
+                # if robot passes over char
+                if norm((pos[0]-robot_pos[-1][0], pos[1]-robot_pos[-1][1])) < 35:
+                    formula.append(chars[i])  # add char to formula
+                    # delete char from list so as to not add twice while in the same circle
+                    del chars_pos[i]
+                    chars = np.delete(chars, i)  # same as above
+
+                # Highlight all the chars
+                cv.circle(frame, (int(pos[0]), int(pos[1])), radius=2, color=(
+                    0, 255, 0), thickness=-1)
+
+            # Highlight the robot positions
+            cv.circle(frame, (int(
+                robot_pos[-1][0]), int(robot_pos[-1][1])), radius=5, color=(0, 0, 255), thickness=-1)
+            for i, _ in enumerate(robot_pos):
+                if i > 0:
+                    p1 = (int(robot_pos[i-1][0]), int(robot_pos[i-1][1]))
+                    p2 = (int(robot_pos[i][0]), int(robot_pos[i][1]))
+                    cv.line(frame, p1, p2, color=(0, 0, 255), thickness=3)
+
+            # Write info
+            info = f"Formula : {formula}"
+            cv.putText(img=frame, text=info, org=(
+                10, height-10), fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=1.5, color=(255, 0, 0), thickness=2)
+
+            # Display the "video"
+            cv.imshow("IAPR project", frame)
+
+            # Frame counter
+            i += 1
 
         # Break the loop
         else:
@@ -67,9 +180,13 @@ def main():
     # When everything done, release the video capture object
     cap.release()
 
+    # Print the result
+    print(f"{formula}")
+
     # Closes all the frames
     cv.destroyAllWindows()
     return
+
 
 if __name__ == "__main__":
     main()
